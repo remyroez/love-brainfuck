a55cb4183f676752d8624c57e9021e5e36f3b75b
れみろう <remyroez+github@gmail.com>
2019-06-19 00:19:34 +0900

    :sparkles: コンフィグを設定

--- game/conf.lua
+++ game/conf.lua
@@ -13,8 +13,8 @@ if love.filesystem then
 	love.filesystem.setRequirePath("lib/?;lib/?.lua;lib/?/init.lua;" .. love.filesystem.getRequirePath())
 end
 
+-- https://love2d.org/wiki/Config_Files
 function love.conf(t)
-	-- https://love2d.org/wiki/Config_Files
-	t.identity = 'love-template'
+	t.identity = 'love-brainfuck'
 	t.version = '11.2'
 end
